c924858d2b582ab82cfedcfeefb9f6d9da7435b9
tonytangdev <tony.tang.dev@gmail.com>
2021-08-20 12:06:48 +0200

    responsive ok

--- components/ResultDisplay.tsx
+++ components/ResultDisplay.tsx
@@ -12,7 +12,9 @@ export const ResultDisplay = (props: Props) => {
         <div className={styles.container}>
             <AmountDisplay price={4.27} title="Tip Amount" subTitle="person"></AmountDisplay>
             <AmountDisplay price={32.79} title="Total" subTitle="person"></AmountDisplay>
-            <Button onClick={e => console.log(e)} text="RESET"></Button>
+            <div className={styles.buttonContainer}>
+                <Button onClick={e => console.log(e)} text="RESET"></Button>
+            </div>
         </div>
     )
 }
