1a07756668c8adee8010a723b504b45ca231a7ec
tonytangdev <tony.tang.dev@gmail.com>
2021-08-20 11:17:31 +0200

    input focus state

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import styles from "../styles/Input.module.css"
 
 interface Props {
@@ -16,12 +17,18 @@ export const Input = ({
     onChange
 }: Props) => {
     const inputId = `${title}-id`
+
+    const [isFocused, setIsFocused] = useState(false)
+
+    const onFocus = () => setIsFocused(value => true)
+    const onBlur = () => setIsFocused(value => false)
+
     return (
         <>
             {!!title && (<label htmlFor={inputId} className={styles.label}>{title}</label>)}
-            <div className={styles.customInput}>
+            <div className={`${styles.customInput} ${!!isFocused && styles.inputIsFocused}`}>
                 <div className={styles.iconContainer}>{icon}</div>
-                <input placeholder={placeholder} type="number" name={inputId} id={inputId} value={value} onChange={onChange} className={styles.input} />
+                <input placeholder={placeholder} type="number" name={inputId} id={inputId} value={value} onChange={onChange} className={styles.input} onFocus={onFocus} onBlur={onBlur} />
             </div>
         </>
     )
